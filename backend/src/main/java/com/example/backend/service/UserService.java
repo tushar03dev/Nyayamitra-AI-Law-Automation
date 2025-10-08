5ee1b03ec6a6752bd7ec85c092ceb9d143befa75
Tushar Sharma <tushar.sharma29sep@gmail.com>
2025-10-08 15:46:25 +0530

    created User Controller

--- backend/src/main/java/com/example/backend/service/UserService.java
+++ backend/src/main/java/com/example/backend/service/UserService.java
@@ -1,9 +1,17 @@
 package com.example.backend.service;
 
+import com.example.backend.model.User;
+import com.example.backend.repo.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
     private UserRepository repo;
     public UserService(UserRepository repo){ this.repo = repo;}
+
+    public List<User> findAll() {
+        return repo.findAll();
+    }
 }
